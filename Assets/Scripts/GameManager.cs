4352421bd036250548b718b7b35357e26191fe44
Dimuthu Dilshan <42347536+dimuthu1997@users.noreply.github.com>
2020-08-10 04:54:09 +0530

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject WinText;
+    public GameObject restartButton;
+
+    int score = 0;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ScoreUp()
+    {
+        score++;
+        if (score >= 4)
+        {
+            Win();
+        }
+
+    }
+    void Win()
+    {
+        WinText.SetActive(true);
+        restartButton.SetActive(true);
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Unity_Corona_Bouncer_Shoot
+# Unity_Corona_Bouncer_Shoot_Game
+A 2D Corona Game made in unity and written in c#
